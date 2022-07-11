63fb6b55262cd34b35f5e30120694234591d21c9
Baines Deal <bainesdeal@gmail.com>
2022-07-11 14:12:17 -0500

    Add project files.

---
+++ WelliDO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WelliDO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
